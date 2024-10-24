cb714f89edab52da517d21f16afccb9201ea7eb0
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-10-23 18:59:20 -0700

    make sure min and max font size are always in the correct sides.
    Add more test

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function formatNumber(value: number, decimalPlaces: number): string {
   const factor = Math.pow(10, decimalPlaces);
@@ -10,11 +10,11 @@ export function formatNumber(value: number, decimalPlaces: number): string {
 export function formatRem(px: number, base: number = 16): string {
   const remValue = px / base; // Convert px to rem
   // Use the formatNumber function to handle rounding
-  return formatNumber(remValue, 3) + 'rem'; // Format to 3 decimal places
+  return formatNumber(remValue, 3) + "rem"; // Format to 3 decimal places
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('extension.clampIt', () => {
+  let disposable = vscode.commands.registerCommand("extension.clampIt", () => {
     const editor = vscode.window.activeTextEditor;
 
     if (editor) {
@@ -22,12 +22,24 @@ export function activate(context: vscode.ExtensionContext) {
       const selectedText = editor.document.getText(selection);
 
       // Parse the selected text
-      const [mobileFontSize, desktopFontSize, customBaseFontSize, customViewportMin, customViewportMax] = selectedText.split(',').map(val => parseFloat(val.trim()));
+      const [
+        mobileFontSize,
+        desktopFontSize,
+        customBaseFontSize,
+        customViewportMin,
+        customViewportMax,
+      ] = selectedText.split(",").map((val) => parseFloat(val.trim()));
 
       // Fetch global settings from the workspace configuration
-      const globalMinViewport = vscode.workspace.getConfiguration().get('clampExtension.globalMinViewport', 600);
-      const globalMaxViewport = vscode.workspace.getConfiguration().get('clampExtension.globalMaxViewport', 1200);
-      const globalBaseFontSize = vscode.workspace.getConfiguration().get('clampExtension.baseFontSize', 16);
+      const globalMinViewport = vscode.workspace
+        .getConfiguration()
+        .get("clampExtension.globalMinViewport", 600);
+      const globalMaxViewport = vscode.workspace
+        .getConfiguration()
+        .get("clampExtension.globalMaxViewport", 1200);
+      const globalBaseFontSize = vscode.workspace
+        .getConfiguration()
+        .get("clampExtension.baseFontSize", 16);
 
       // Check if a custom base font is provided; if not, use the global base font size
       const baseFontSize = customBaseFontSize || globalBaseFontSize;
@@ -37,10 +49,16 @@ export function activate(context: vscode.ExtensionContext) {
       const viewportMax = customViewportMax || globalMaxViewport;
 
       // Generate the clamp function in rem using the effective base font size
-      const clampFunction = generateClampFunction(baseFontSize, mobileFontSize, desktopFontSize, viewportMin, viewportMax);
+      const clampFunction = generateClampFunction(
+        baseFontSize,
+        mobileFontSize,
+        desktopFontSize,
+        viewportMin,
+        viewportMax
+      );
 
       // Replace the selected text with the clamp function
-      editor.edit(editBuilder => {
+      editor.edit((editBuilder) => {
         editBuilder.replace(selection, clampFunction);
       });
     }
@@ -50,19 +68,32 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // Function to generate the clamp() function in rem
-export function generateClampFunction(baseFontSize: number, mobileFontSize: number, desktopFontSize: number, viewportMin: number, viewportMax: number): string {
+export function generateClampFunction(
+  baseFontSize: number,
+  mobileFontSize: number,
+  desktopFontSize: number,
+  viewportMin: number,
+  viewportMax: number
+): string {
+  // set Min and Max font size for clamp function
+  const minFontSize = Math.min(mobileFontSize, desktopFontSize);
+  const maxFontSize = Math.max(mobileFontSize, desktopFontSize);
+
   // Convert all values to rem using the specified base font size
-  const baseFontSizeRem = formatRem(baseFontSize, baseFontSize);
-  const mobileFontSizeRem = formatRem(mobileFontSize, baseFontSize);
-  const desktopFontSizeRem = formatRem(desktopFontSize, baseFontSize);
+  const minFontSizeRem = formatRem(minFontSize, baseFontSize);
+  const maxFontSizeRem = formatRem(maxFontSize, baseFontSize);
 
   // Calculate slope and intersection in rem
-  const slope = (desktopFontSize - mobileFontSize) / (viewportMax - viewportMin);
+  const slope =
+    (desktopFontSize - mobileFontSize) / (viewportMax - viewportMin);
   const yAxisIntersection = -viewportMin * slope + mobileFontSize;
   const vw = slope * 100;
 
   // Generate clamp string in rem
-  return `clamp(${mobileFontSizeRem}, calc(${formatRem(yAxisIntersection, baseFontSize)} + ${formatNumber(vw, 3)}vw), ${desktopFontSizeRem})`;
+  return `clamp(${minFontSizeRem}, calc(${formatRem(
+    yAxisIntersection,
+    baseFontSize
+  )} + ${formatNumber(vw, 3)}vw), ${maxFontSizeRem})`;
 }
 
 export function deactivate() {}
